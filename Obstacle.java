4743c7f24b5ba82f39c10a2023533cdc711e36cd
Kaan Türkoğlu <kaanturkoglu2003@hotmail.com>
2024-08-05 14:48:05 +0300

    fix: Obstacle class usage and assets

--- Obstacle.java
+++ Obstacle.java
@@ -2,21 +2,37 @@ import javafx.scene.image.Image;
 
 public abstract class Obstacle {
 
-    Image grass = new Image("file:/Users/kaanturkoglu/Desktop/grass20.png");
-    Image sand = new Image("file:/Users/kaanturkoglu/Desktop/sandtile.png");
-    Image water = new Image("file:/Users/kaanturkoglu/Desktop/water.gif");
-    Image mountain = new Image("file:/Users/kaanturkoglu/Desktop/mountile.png");
-    Image forest = new Image("file:/Users/kaanturkoglu/Desktop/lowObs.png");
-    Image friendlyObs = new Image("file:/Users/kaanturkoglu/Desktop/friendsObs.png");
-    Image enemyObs = new Image("file:/Users/kaanturkoglu/Desktop/enemysObs.png");
+    // Image grass = new Image("file:/Users/kaanturkoglu/Desktop/grass20.png");
+    // Image sand = new Image("file:/Users/kaanturkoglu/Desktop/sandtile.png");
+    // Image water = new Image("file:/Users/kaanturkoglu/Desktop/water.gif");
+    // Image mountain = new Image("file:/Users/kaanturkoglu/Desktop/mountile.png");
+    // Image forest = new Image("file:/Users/kaanturkoglu/Desktop/lowObs.png");
+    // Image friendlyObs = new Image("file:/Users/kaanturkoglu/Desktop/friendsObs.png");
+    // Image enemyObs = new Image("file:/Users/kaanturkoglu/Desktop/enemysObs.png");
 
-    private int obstacleType; // 0 for clear path, 1 for mountain, 2 for forest, 3 for water, 4 for sand, 5
+    // Image grass = new Image("file:/Users/kaanturkoglu/Desktop/grass20.png");
+    // Image sand = new Image("file:/Users/kaanturkoglu/Desktop/sandtile.png");
+    // Image water = new Image("file:/Users/kaanturkoglu/Desktop/water.gif");
+    // Image mountain = new Image("file:/Users/kaanturkoglu/Desktop/mountile.png");
+    // Image forest = new Image("file:/Users/kaanturkoglu/Desktop/lowObs.png");
+    // Image friendlyObs = new Image("file:/Users/kaanturkoglu/Desktop/friendsObs.png");
+    // Image enemyObs = new Image("file:/Users/kaanturkoglu/Desktop/enemysObs.png");
+
+    Image grass = new Image(getClass().getResourceAsStream("/Assets/grass20.png"));
+    Image sand = new Image(getClass().getResourceAsStream("/Assets/sandtile.png"));
+    Image water = new Image(getClass().getResourceAsStream("/Assets/water.gif"));
+    Image mountain = new Image(getClass().getResourceAsStream("/Assets/mountile.png"));
+    Image forest = new Image(getClass().getResourceAsStream("/Assets/lowObs.png"));
+    Image friendlyObs = new Image(getClass().getResourceAsStream("/Assets/friendsObs.png"));
+    Image enemyObs = new Image(getClass().getResourceAsStream("/Assets/enemysObs.png"));
+
+    private String obstacleType; // 0 for clear path, 1 for mountain, 2 for forest, 3 for water, 4 for sand, 5
                               // for friendly, 6 for enemy
     private int height;
     private String obsSide; // neutral, friendly, enemy
     Image obstacleImage;
 
-    public Obstacle(int obstacleType, int height, String obsSide) {
+    public Obstacle(String obstacleType, int height, String obsSide) {
         this.obstacleType = obstacleType;
         this.height = height;
         this.obsSide = obsSide;
@@ -26,11 +42,11 @@ public abstract class Obstacle {
 
     }
 
-    public int getObstacleType() {
+    public String getObstacleType() {
         return obstacleType;
     }
 
-    public void setObstacleType(int obstacleType) {
+    public void setObstacleType(String obstacleType) {
         this.obstacleType = obstacleType;
     }
 
@@ -59,11 +75,21 @@ public abstract class Obstacle {
     }
 }
 
+class Grass extends Obstacle {
+    public Grass() {
+        super();
+        this.setHeight(100);
+        this.setObstacleType("grass");
+        this.setObsSide("neutral");
+        this.obstacleImage = grass;
+    }
+}
+
 class Mountain extends Obstacle {
     public Mountain() {
         super();
         this.setHeight(5137);
-        this.setObstacleType(1);
+        this.setObstacleType("mountain");
         this.setObsSide("neutral");
         this.obstacleImage = mountain;
     }
@@ -73,27 +99,18 @@ class Forest extends Obstacle {
     public Forest() {
         super();
         this.setHeight(2117);
-        this.setObstacleType(2);
+        this.setObstacleType("forest");
         this.setObsSide("neutral");
         this.obstacleImage = forest;
     }
 }
 
-class Grass extends Obstacle {
-    public Grass() {
-        super();
-        this.setHeight(100);
-        this.setObstacleType(0);
-        this.setObsSide("neutral");
-        this.obstacleImage = grass;
-    }
-}
 
 class Water extends Obstacle {
     public Water() {
         super();
         this.setHeight(0);
-        this.setObstacleType(3);
+        this.setObstacleType("water");
         this.setObsSide("neutral");
         this.obstacleImage = water;
     }
@@ -103,7 +120,7 @@ class Sand extends Obstacle {
     public Sand() {
         super();
         this.setHeight(0);
-        this.setObstacleType(4);
+        this.setObstacleType("sand");
         this.setObsSide("neutral");
         this.obstacleImage = sand;
     }
@@ -113,7 +130,7 @@ class FriendlyObstacle extends Obstacle {
     public FriendlyObstacle() {
         super();
         this.setHeight(0);
-        this.setObstacleType(5);
+        this.setObstacleType("friendlyObs");
         this.setObsSide("friendly");
         this.obstacleImage = friendlyObs;
     }
@@ -123,7 +140,7 @@ class EnemyObstacle extends Obstacle {
     public EnemyObstacle() {
         super();
         this.setHeight(0);
-        this.setObstacleType(6);
+        this.setObstacleType("enemyObs");
         this.setObsSide("enemy");
         this.obstacleImage = enemyObs;
     }
